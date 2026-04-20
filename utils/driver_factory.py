70bfeb6ae9c01a3c36278f0e31f8a22abab76404
InventoApp <helpinghandselmhurst@gmail.com>
2026-04-20 12:17:22 +0530

    new

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -2,7 +2,6 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 import os
 
-
 def create_driver():
     options = UiAutomator2Options()
 
@@ -10,8 +9,12 @@ def create_driver():
     options.device_name = "emulator-5554"
     options.automation_name = "UiAutomator2"
 
-    # APK will be downloaded by pipeline
     options.app = os.path.abspath("app.apk")
+
+    # 🔥 FIX: increase timeout (VERY IMPORTANT)
+    options.set_capability("uiautomator2ServerInstallTimeout", 120000)
+    options.set_capability("adbExecTimeout", 120000)
+    options.set_capability("androidInstallTimeout", 120000)
 
     driver = webdriver.Remote(
         command_executor="http://127.0.0.1:4723",
