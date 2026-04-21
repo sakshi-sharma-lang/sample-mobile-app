2afb85b9a31d4ed4f73222e37eb523966cd4e001
InventoApp <helpinghandselmhurst@gmail.com>
2026-04-21 10:08:19 +0530

    new

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,5 @@
-from utils.driver_factory import create_driver
+def test_fail():
+    assert False
 
-
-def test_app_launch():
-    driver = create_driver()
-
-    # Basic validation: app launched
-    current_package = driver.current_package
-    print("Current package:", current_package)
-
-    assert current_package is not None
-
-    driver.quit()
-
+# def test_fail():
+#     assert True
